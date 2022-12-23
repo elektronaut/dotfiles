c0b4bc3ba41265a3993d8a4930aab6272953a4e7
Inge Jørgensen <inge@elektronaut.no>
2022-12-24 00:52:27 +0100

    oh-my-zsh tweaks

--- run_once_before_install-oh-my-zsh.sh
+++ run_once_before_install-oh-my-zsh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
